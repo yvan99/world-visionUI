28fc2f774f001157443e8d6a0dccc4aaeb359d9f
yvan99 <ishimweyvan90@gmail.com>
2021-07-03 13:51:24 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        worldVisionColor: {
+          500: "#ff6b00",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
